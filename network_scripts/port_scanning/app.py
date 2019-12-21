0b5ab1e654bb71c52c538a239c6df196137b4bd0
ben <ben.gabay38@gmail.com>
2019-12-21 19:36:12 +0200

    refacotor the port scanning code to be more generic

--- network_scripts/port_scanning/app.py
+++ network_scripts/port_scanning/app.py
@@ -1,21 +1,40 @@
 from scapy.all import *
-from port_scanning import config
+import datetime
+
+PACKET_TIMEOUT = 2
 
 
-def main():
-    print(config.OPENING_MESSAGE % config.HOST)
-    port = config.MIN_PORT
-    while port <= config.MAX_PORT:
-        my_packet = IP(dst=config.HOST) / TCP(sport=config.SOURCE_PORT, dport=port)
-        response_packet = sr1(my_packet, timeout=config.PACKET_TIMEOUT, verbose=config.PACKET_VERBOSE)
-        if response_packet is None:
-            sys.stdout.write(config.RED)
-            print(config.CLOSE_PORT_MESSAGE % port)
-        else:
-            sys.stdout.write(config.GREEN)
-            print(config.OPEN_PORT_MESSAGE % port)
-        port += 1
+def print_status_message(port, is_open):
+    if is_open:
+        message = f"port {port} is open."
+    else:
+        message = f"port {port} is close."
+    print(message)
 
 
-if __name__ == '__main__':
-    main()
+def open_port(target, port):
+    my_packet = IP(dst=target) / TCP(sport=port, dport=port)
+    response_packet = sr1(my_packet, timeout=PACKET_TIMEOUT, verbose=False)
+    return bool(response_packet)
+
+
+def timeout_override(start_time, timeout):
+    if timeout:
+        timeout = datetime.timedelta(seconds=timeout)
+        current_time = datetime.datetime.now()
+        return start_time + timeout <= current_time
+    return False
+
+
+def port_scanning(target, min_port=1, max_port=65536, timeout=None, verbose=True):
+    ports = {}
+    start_time = datetime.datetime.now()
+    for current_port in range(min_port, max_port):
+        if timeout_override(start_time, timeout):
+            break
+        is_open = open_port(target, current_port)
+        if verbose:
+            print_status_message(current_port, is_open)
+        ports[str(current_port)] = {"open": is_open}
+        current_port += 1
+    return ports
