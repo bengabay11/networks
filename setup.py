583444326e62d77dd833145b56e7d9eab5a51ac1
ben <ben.gabay38@gmail.com>
2019-12-21 23:49:16 +0200

    renamed the project to networks instead of network_scrips

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="network_scrips",
+    name="networks",
     version="0.0.1",
     author="Ben Gabay",
     author_email="ben.gabay38@example.com",
-    description="Useful network scripts",
+    description="Network operations for humans",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/bengabay11/network-scripts",
+    url="https://github.com/bengabay11/networks",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
