92a91e0e31d22e35081374287921bb6f7e16d61a
ben <ben.gabay38@gmail.com>
2019-12-22 22:48:48 +0200

    added docstring in init file

--- networks/__init__.py
+++ networks/__init__.py
@@ -1,3 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+    networks
+    ~~~~~
+
+    networks is a package for performing network operations.
+    results as python objects.
+
+    :copyright: 2019 Ben Gabay
+    :license: MIT, see LICENSE for more details.
+"""
+
 from .ping import ping
 from .arp_spoofing import arp_spoofing
 from .hosts_in_segment import get_hosts_in_segment
